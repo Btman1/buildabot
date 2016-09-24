bf3f3b1e0ec3bc73ed4b90a6ec419c0ba4df012e
Btman1 <allentyao@gmail.com>
2016-09-23 19:14:23 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ gameInput.keydown(function(keydownEvent) {
 
 function parseText(text) {    
     if (text === "hello") {
-      gameOutput.html("Goodbye
-                      ");
+      gameOutput.html("Goodbye");
     }
 }
 
